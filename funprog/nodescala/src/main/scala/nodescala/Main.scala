85abd738fe5c7b14d96fe9ddab43178c14561846
Benjamin Yu <benjamin.sichit.yu@gmail.com>
2014-04-24 22:14:03 -0400

    trying out server

--- funprog/nodescala/src/main/scala/nodescala/Main.scala
+++ funprog/nodescala/src/main/scala/nodescala/Main.scala
@@ -5,6 +5,7 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import ExecutionContext.Implicits.global
 import scala.async.Async.{async, await}
+import scala.util._
 
 object Main {
 
@@ -13,27 +14,33 @@ object Main {
     // 1. instantiate the server at 8191, relative path "/test",
     //    and have the response return headers of the request
     val myServer = new NodeScala.Default(8191)
-    val myServerSubscription = ???
+    val myServerSubscription = myServer.createListener("/test").start()
 
     // TO IMPLEMENT
     // 2. create a future that expects some user input `x`
     //    and continues with a `"You entered... " + x` message
-    val userInterrupted: Future[String] = ???
+    val userInterrupted: Future[String] = Future.userInput("You entered...")
 
     // TO IMPLEMENT
     // 3. create a future that completes after 20 seconds
     //    and continues with a `"Server timeout!"` message
-    val timeOut: Future[String] = ???
+    val timeOut: Future[String] = Future.delay(Duration("20seconds")).continue {
+      case Success(v) => "Server timeout!"
+    }
 
     // TO IMPLEMENT
     // 4. create a future that completes when either 10 seconds elapse
     //    or the user enters some text and presses ENTER
-    val terminationRequested: Future[String] = ???
+    val terminationRequested: Future[String] = Future.any(List(userInterrupted,timeOut))
 
     // TO IMPLEMENT
     // 5. unsubscribe from the server
     terminationRequested onSuccess {
-      case msg => ???
+      case msg => {
+        print(msg)
+        myServerSubscription.unsubscribe()
+        print("BYE!")
+      }
     }
   }
 
